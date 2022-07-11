fab5b20d177f953e61ab09fcc2b9ad7d9a136195
Cole Ferguson <fergusoncole98@outlook.com>
2022-07-11 13:42:48 -0500

    Initial Commit on Overloading

---
+++ Method Overloading Exercise1/Method1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading_Exercise1
+{
+    internal class Method1
+    {
+		public static int Add(int x, int y)
+		{
+			return x + y;
+		}
+
+		public static decimal Add(decimal x, decimal y)
+		{
+			return x + y;
+		}
+
+		public static string Add(int x, int y, bool isCheck)
+		{
+			var sum = x + y;
+
+			if (isCheck == true && sum > 1)
+			{
+				return $"{sum} dollars";
+			}
+			else if (isCheck == true && sum == 1)
+			{
+				return $"{sum} dollar";
+			}
+			else if (isCheck == true && sum < 1)
+			{
+				return $"{sum} dollars";
+			}
+			else
+			{
+				return sum.ToString();
+			}
+		}
+	}
+}
---
+++ Method Overloading Exercise1/Program.cs
@@ -0,0 +1,4 @@
+﻿using Method_Overloading_Exercise1;
+
+var answer = Method1.Add(20, 3, true);
+Console.WriteLine(answer);
